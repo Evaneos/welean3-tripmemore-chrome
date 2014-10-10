e0cbd2f9a087c31da3208c4c62e477e05b5354c8
Michael FORASTE <michael.foraste@gmail.com>
2014-10-10 11:22:26 +0200

    Adding settings page

--- scripts/iconActions.js
+++ scripts/iconActions.js
@@ -1,3 +1,5 @@
+console.log('Oh ! Man ! You loaded me !')
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-  console.log('Oh ! Man ! You clicked me')
+  console.log('Oh ! Man ! You clicked me !')
 });
