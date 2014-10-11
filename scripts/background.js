8c09a581c4c0017b69ede2e13824268cc12659d8
Michael FORASTE <michael.foraste@gmail.com>
2014-10-11 16:51:04 +0200

    adding lightbox style

--- scripts/background.js
+++ scripts/background.js
@@ -1,5 +1,10 @@
 chrome.contextMenus.create({
-  "title": "Pin this",
-  "contexts": ["image"],
-  "onclick" : function () {}
+  title: "Pin this",
+  contexts: ["image"],
+  onclick : function (info) {
+  	chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+			chrome.tabs.executeScript(tabs[0].id, {file:"scripts/lightbox.js"});
+			chrome.tabs.sendMessage(tabs[0].id, {type: "startLightbox"});
+		});
+  }
 });
