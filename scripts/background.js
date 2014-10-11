90be7b8bd5bc837ed70c29fd0afcd17f4abe388c
Michael FORASTE <michael.foraste@gmail.com>
2014-10-11 16:56:38 +0200

    changing style + starting background management

--- scripts/background.js
+++ scripts/background.js
@@ -3,7 +3,6 @@ chrome.contextMenus.create({
   contexts: ["image"],
   onclick : function (info) {
   	chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-			chrome.tabs.executeScript(tabs[0].id, {file:"scripts/lightbox.js"});
 			chrome.tabs.sendMessage(tabs[0].id, {type: "startLightbox"});
 		});
   }
